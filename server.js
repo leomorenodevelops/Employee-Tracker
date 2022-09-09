8bff7767598c8000b273ac4d691a1462fafebad2
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-09-09 15:34:37 -0600

    Made viewAllEmployees function

--- server.js
+++ server.js
@@ -63,3 +63,12 @@ function start() {
             }
         });
 }
+
+// Function for View All Employees
+function viewAllEmployees() {
+    connection.query('SELECT e.id AS ID, e.first_name AS First, e.last_name AS Last, e.role_id AS Role, r.salary AS Salary, m.last_name AS Manager, d.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role r ON e.role_id = r.title LEFT JOIN department d on r.department_id = d.id', function(err, results) {
+        if(err) throw err;
+        console.table(results);
+        start();
+    });
+}
